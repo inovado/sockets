91614d5ff2a1ac717c461b38a6b051810756a68d
Drack <nautilus_com@yahoo.com>
2020-12-25 23:04:14 -0600

    creacion de pagina home

---
+++ lib/pages/home.dart
@@ -0,0 +1,15 @@
+//fl-page  CREA UN STATELESS WIDGET
+import 'package:flutter/material.dart';
+
+
+class HomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Hola Mundo'),
+     ),
+   );
+  }
+}
